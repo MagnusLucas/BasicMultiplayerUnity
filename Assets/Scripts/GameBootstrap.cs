df0473b305293a06c20277c9fbd065051d19d42b
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-07-22 16:37:47 +0200

    Starting the client-server connection

---
+++ Assets/Scripts/GameBootstrap.cs
@@ -0,0 +1,10 @@
+using Unity.NetCode;
+using UnityEngine;
+
+[UnityEngine.Scripting.Preserve]
+public class NewMonoBehaviourScript : ClientServerBootstrap {
+    public override bool Initialize(string defaultWorldName) {
+        AutoConnectPort = 7979;
+        return base.Initialize(defaultWorldName);
+    }
+}
